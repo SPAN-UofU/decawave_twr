82d98b79b326e1a3a1cd1d3cf3b2dad01a3e0615
Anh Luong <luong@eng.utah.edu>
2016-10-19 09:33:13 -0700

    create minskowski, frequency sync demo

--- overlay/build_overlay.sh
+++ overlay/build_overlay.sh
@@ -5,3 +5,4 @@ echo "Compiling the overlay from .dts to .dtbo"
 dtc -O dtb -o DW1000-SPI0-00A0.dtbo -b 0 -@ DW1000-SPI0-00A0.dts
 dtc -O dtb -o DW1000-SPI1-00A0.dtbo -b 0 -@ DW1000-SPI1-00A0.dts
 sudo mv *.dtbo /export/rootfs/lib/firmware
+#sudo mv *.dtbo /lib/firmware
